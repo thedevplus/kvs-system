fd56e4fd58121eb3adb595e647c8e9147b459946
thedevplus <287428056+thedevplus@users.noreply.github.com>
2026-06-11 22:50:41 +0800

    fix: move CLI implementation from kvs-server to kvs-client

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,49 +1,5 @@
-use clap::Parser;
-use kvs::{Result, KvStore, KvsEngine};
-use std::{env, process};
-
-#[derive(Parser)]
-#[command(version)]
-struct Args {
-    command: Option<String>,
-    key: Option<String>,
-    value: Option<String>,
-}
+use kvs::Result;
 
 fn main() -> Result<()> {
-    let args = Args::parse();
-    let mut kvs = KvStore::open(&env::current_dir()?)?;
-
-    match args.command.unwrap().as_ref() {
-        "-V" => println!("{}", env!("CARGO_PKG_VERSION")),
-        "set" => {
-            if let Some(value) = args.value {
-                kvs.set(args.key.unwrap(), value)?;
-            } else {
-                process::exit(1);
-            }
-        }
-        "get" => {
-            if args.value.is_some() {
-                process::exit(1);
-                //return Err(kvs::error::KvError::Other);
-            } else {
-                let Ok(Some(_)) = kvs.get(args.key.unwrap()) else {
-                    process::exit(0);
-                };
-            }
-        }
-        "rm" => {
-            if args.value.is_some() {
-                process::exit(1);
-            } else {
-                let Ok(_) = kvs.remove(args.key.unwrap()) else {
-                    process::exit(1);
-                };
-            }
-        }
-        _ => process::exit(1),
-    }
-
     Ok(())
 }
