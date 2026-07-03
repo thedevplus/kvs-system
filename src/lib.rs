b3d1a14436374c35d66833ca13f3b58938b8176b
thedevplus <287428056+thedevplus@users.noreply.github.com>
2026-07-03 23:59:35 +0800

    feat: add Engine enum wrapper for dyn KvsEngine compatibility

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,31 @@ pub type Result<T> = std::result::Result<T, KvError>;
 pub use crate::engine::KvsEngine;
 pub use crate::kvs::KvStore;
 pub use crate::sled::SledKvsEngine;
+
+pub enum Engine {
+    Kvs(KvStore),
+    Sled(SledKvsEngine),
+}
+
+impl Engine {
+    pub fn set(&self, key: String, value: String) -> Result<()> {
+        match self {
+            Engine::Kvs(kvs) => kvs.set(key, value),
+            Engine::Sled(sled) => sled.set(key, value),
+        }
+    }
+
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        match self {
+            Engine::Kvs(kvs) => kvs.get(key),
+            Engine::Sled(sled) => sled.get(key),
+        }
+    }
+
+    pub fn remove(&self, key: String) -> Result<()> {
+        match self {
+            Engine::Kvs(kvs) => kvs.remove(key),
+            Engine::Sled(sled) => sled.remove(key),
+        }
+    }
+}
